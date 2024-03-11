d1a0ef2a2ed31b74d0f34a8a8fe34bfa78e7aaa9
clausmart007 <107912244+clausmart007@users.noreply.github.com>
2024-03-10 19:19:32 -0600

    Versión final juego

---
+++ desafios.js
@@ -0,0 +1,75 @@
+let listaGenerica = [];
+let lenguagesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+lenguagesDeProgramacion.push('Java', 'Ruby', 'GoLang');
+
+function mostrarLenguagesSeparadamente() {
+    for (let i = 0; i < lenguagesDeProgramacion.length; i++) {
+      console.log(lenguagesDeProgramacion[i]);
+    }
+  }
+  
+  mostrarLenguagesSeparadamente();
+
+  function mostrarLenguagesReversoSeparadamente() {
+    for (let i = lenguagesDeProgramacion.length - 1; i >= 0; i--) {
+      console.log(lenguagesDeProgramacion[i]);
+    }
+  }
+  
+  mostrarLenguagesReversoSeparadamente();
+
+  function calcularMedia(lista) {
+    let suma = 0;
+    for (let i = 0; i < lista.length; i++) {
+      suma += lista[i];
+    }
+    return suma / lista.length;
+  }
+  
+  let numeros = [10, 20, 30, 40, 50];
+  let media = calcularMedia(numeros);
+  console.log('Média:', media);
+
+
+
+  function encontrarMayorMenor(lista) {
+    let mayor = lista[0];
+    let menor = lista[0];
+  
+    for (let i = 1; i < lista.length; i++) {
+      if (lista[i] > mayor) {
+        mayor = lista[i];
+      }
+      if (lista[i] < menor) {
+        menor = lista[i];
+      }
+    }
+  
+    console.log('Mayor:', mayor);
+    console.log('Menor:', menor);
+  }
+  
+  let numeros = [15, 8, 25, 5, 12];
+  encontrarMayorMenor(numeros);
+
+  function calcularSuma(lista) {
+    let suma = 0;
+    for (let i = 0; i < lista.length; i++) {
+      suma += lista[i];
+    }
+    return suma;
+  }
+  
+  let numeros = [15, 8, 25, 5, 12];
+  let suma = calcularSuma(numeros);
+  console.log('Suma:', suma);
+
+
+  function encontrarIndiceElemento(lista, elemento) {
+    for (let i = 0; i < lista.length; i++) {
+      if (lista[i] === elemento) {
+        return i; // Retorna el índice del elemento encontrado
+      }
+    }
+    return -1; // Retorna -1 si el elemento no se encuentra en la lista
+  }
